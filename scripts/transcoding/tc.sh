126b427beb96ae24a9d0e252f085b48cbc58a047
Aaron H <tercelhuang@gmail.com>
2016-04-09 17:30:48 -0700

    Shell script practice, transcoding GBK to UTF-8

---
+++ scripts/transcoding/tc.sh
@@ -0,0 +1,28 @@
+
+
+
+# echo "Positional Parameters"
+# echo '$0 = ' $0
+# echo '$1 = ' $1
+
+if [ "$1" != "" ]; then
+    search_dir="$1";
+    echo "Start trans-coding..."
+else
+    echo "Please provide a path for target folder..."
+fi
+
+# folder for result
+mkdir "$search_dir/result"
+
+# file_type = ".txt"
+for entry in "$search_dir"/*.txt
+do
+  # echo "$entry"
+  fname=`basename $entry`
+  echo $fname
+  # file -I $entry
+  iconv -c -f GB2312 -t UTF-8 $entry >> "$search_dir/result/$fname"
+done
+
+echo "Trans-coding complete, check out $search_dir/result folder."
