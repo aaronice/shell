8c81dbf8655229a0f2028e45824babbaac73773f
aaronice <tercelhuang@gmail.com>
2016-04-09 23:34:14 -0700

    modify logic for fault tolerance

--- scripts/transcoding/tc.sh
+++ scripts/transcoding/tc.sh
@@ -7,21 +7,22 @@
 if [ "$1" != "" ]; then
     search_dir="$1";
     echo "Start trans-coding..."
+
+    # folder for result
+    mkdir "$search_dir/result"
+
+    # file_type = ".txt"
+    for entry in "$search_dir"/*.txt
+    do
+      # echo "$entry"
+      fname=`basename $entry`
+      echo $fname
+      # file -I $entry
+      iconv -c -f GB2312 -t UTF-8 $entry >> "$search_dir/result/$fname"
+    done
+
+    echo "Trans-coding complete, check out $search_dir/result folder."
+
 else
     echo "Please provide a path for target folder..."
 fi
-
-# folder for result
-mkdir "$search_dir/result"
-
-# file_type = ".txt"
-for entry in "$search_dir"/*.txt
-do
-  # echo "$entry"
-  fname=`basename $entry`
-  echo $fname
-  # file -I $entry
-  iconv -c -f GB2312 -t UTF-8 $entry >> "$search_dir/result/$fname"
-done
-
-echo "Trans-coding complete, check out $search_dir/result folder."
